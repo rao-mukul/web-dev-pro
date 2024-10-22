73800911368da13ce433577812cce9a24c014829
rao-mukul <mukultheprogrammer@gmail.com>
2024-10-22 23:29:50 +0530

    printing in JS

---
+++ jsFoundation/part1/printing.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+console.log("chai");
+process.stdout.write("chai");
+process.stdout.write("chai");
+console.log("Table");
+
+console.table({ city: "jaipur" });
+
+console.log("Log");
+console.warn("Warning");
+console.error("Error");
