8709d0bf1fed4bdbdf43fbe9fe7ea9a676302376
rao-mukul <mukultheprogrammer@gmail.com>
2024-10-22 23:59:19 +0530

    data types

---
+++ jsFoundation/part1/datatype.js
@@ -0,0 +1,32 @@
+console.log("Hello");
+// console.log("chai");
+
+/*
+
+String
+Number
+Boolean
+Bigint
+
+Undefined 
+null
+
+Object
+
+Symbol
+
+*/
+
+// var score = 101;
+
+let score = 101;
+let name = "Mukul"
+let isLoggedin = true
+
+//Object
+let teaTypes = ["lemon","orange"]
+let user = {firstname:"Mukul", lastname: "Yadav"}
+
+let getScore = score
+
+console.log(getScore);
