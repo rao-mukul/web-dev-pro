3bf2217088abcb7b3fd0e1e368ed448919071003
rao-mukul <mukultheprogrammer@gmail.com>
2024-10-25 18:44:35 +0530

    assignment operators in JS

---
+++ jsFoundation/part1/asignment.js
@@ -0,0 +1,22 @@
+let score = 10 + 11;
+
+let num = 10;
+
+// num = num + 5;
+num += 5;
+console.log(num);
+
+num -= 3;
+console.log(num);
+
+num /= 2;
+console.log(num);
+
+num *= 5;
+console.log(num);
+
+num %= 4;
+console.log(num);
+
+num **= 4;
+console.log(num);
