5f257265591cab32fc17f29c722b3b9ba985e352
rao-mukul <mukultheprogrammer@gmail.com>
2024-10-25 18:17:43 +0530

    fun with operations

--- jsFoundation/part1/operations.js
+++ jsFoundation/part1/operations.js
@@ -3,11 +3,17 @@ let bonus = 25;
 
 let totalScore = score + bonus;
 
+console.log("The Sum of " + score + " and " + bonus + " is: " + totalScore);
+
 let addition = 4 + 5;
 let subtract = 9 - 3;
 let mult = 3 * 5;
-let divi = 8 / 2;
+let divi = 9 / 2;
+console.log("The divsion is: " + divi);
+
 let reminder = 9 % 2;
+console.log("The reminder is: " + reminder);
+
 let expo = 2 ** 3;
 
 let myscore = 110;
@@ -16,7 +22,7 @@ myscore++;
 let credits = 56;
 credits--;
 
-// Comparion operation
+// Comparison operation
 
 let num1 = 3;
 let num2 = 3;
@@ -24,5 +30,5 @@ let num3 = 6;
 
 console.log(num1 == num2); //true
 console.log(num1 != num3); // true
-console.log(num1 > num3); // true
+console.log(num1 > num3); // flase
 console.log(num1 < num3); // true
