a8fe210303a3b4eeca6649ac17a968cfbe78cb47
rao-mukul <mukultheprogrammer@gmail.com>
2024-10-25 18:33:18 +0530

    logical operators in JS

---
+++ jsFoundation/part1/logical.js
@@ -0,0 +1,14 @@
+// && -> And
+// !! -> Or
+// ! -> Reverse
+
+let isLoggedin = true;
+let isPaid = false;
+console.log(isLoggedin && isPaid);
+
+let isEmailUser = true;
+let isGoogleUser = false;
+console.log(isEmailUser || isGoogleUser);
+
+const isTrue = true;
+console.log(!isTrue);
