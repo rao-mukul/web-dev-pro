45e4e854e6dc837cce9ead550daf704ee5119fbf
rao-mukul <mukultheprogrammer@gmail.com>
2024-10-25 19:03:42 +0530

    operator precendence in JS

---
+++ jsFoundation/part1/opePre.js
@@ -0,0 +1,6 @@
+let score = 2 * 3 + 2 - 1;
+console.log(score);
+
+// Use Parentheses, makes the code more readable and gives Predictability to the code
+score = ((2 * (3 + 2)) - 1);
+console.log(score);
