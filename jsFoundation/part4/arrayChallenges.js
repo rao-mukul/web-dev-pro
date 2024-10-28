bf71cef79c3f936391c5c85eabc73d972c30e59d
rao-mukul <mukultheprogrammer@gmail.com>
2024-10-28 23:26:55 +0530

    solve 5 array challenges

--- jsFoundation/part4/arrayChallenges.js
+++ jsFoundation/part4/arrayChallenges.js
@@ -7,3 +7,42 @@ let teaFlavors = ["green tea", "black tea", "oolong tea"];
 
 const firstTea = teaFlavors[0];
 // console.log(firstTea);
+
+/* 
+2. Declare an array named `cities` containing `"London"`, `"Tokyo"`, `"Paris"`, and `"New York"`. 
+   Access the third element in the array and store it in a variable named `favoriteCity`.
+*/
+
+let cities = ["London", "Tokyo", "Paris", "New York"];
+
+let favoriteCity = cities[2];
+// console.log(favoriteCity);
+
+/* 
+3. You have an array named `teaTypes` containing `"herbal tea"`, `"white tea"`, and `"masala chai"`. 
+   Change the second element of the array to `"jasmine tea"`.
+*/
+
+let teaTypes = ["herbal tea", "white tea", "masala chai"];
+
+teaTypes[1] = "jasmine tea";
+// console.log(teaTypes);
+
+/* 
+4. Declare an array named `citiesVisited` containing `"Mumbai"` and `"Sydney"`. 
+   Add `"Berlin"` to the array using the `push` method.
+*/
+
+let citiesVisited = ["Mumbai", "Sydney"];
+
+citiesVisited.push("Berlin");
+// console.log(citiesVisited);
+
+/* 
+5. You have an array named `teaOrders` with `"chai"`, `"iced tea"`, `"matcha"`, and `"earl grey"`. 
+   Remove the last element of the array using the `pop` method and store it in a variable named `lastOrder`.
+*/
+
+let teaOrders = ["chai", "iced tea", "matcha", "earl grey"];
+let lastOrder = teaOrders.pop();
+// console.log(lastOrder);
