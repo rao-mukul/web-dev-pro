75f6083bf32aef0cca38326700d7bea880ca74ff
rao-mukul <mukultheprogrammer@gmail.com>
2024-11-04 23:10:42 +0530

    loop challenge 3 + Input from user in JS

--- jsFoundation/part4/loopChallenges.js
+++ jsFoundation/part4/loopChallenges.js
@@ -24,4 +24,22 @@ while (j >= 1) {
 }
 // console.log(countdown);
 
+/* 
+3. Write a `do while` loop that prompts a user to enter their favorite tea type until they enter `"stop"`. 
+   Store each tea type in an array named `teaCollection`.
+*/
 
+let teaCollection = [];
+let tea;
+
+// do {
+//   tea = prompt(`Enter your faviorite Tea (type:"stop" to Exit)`);
+
+//   if (tea !== "stop") {
+//     teaCollection.push(tea);
+//   }
+// } while (tea !== "stop");
+
+/* 
+4. Write a `do while` loop that adds numbers from 1 to 3 and stores the result in a variable named `total`.
+*/
