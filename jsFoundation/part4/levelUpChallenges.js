85d454b0a1ad165f81ca9ca9dcbc6c754ca06baf
rao-mukul <mukultheprogrammer@gmail.com>
2024-11-04 23:47:52 +0530

    level up challenge 1: break keyword

---
+++ jsFoundation/part4/levelUpChallenges.js
@@ -0,0 +1,15 @@
+/* 
+1. Write a `for` loop that loops through the array `["green tea", "black tea", "chai", "oolong tea"]` and stops the loop when it finds `"chai"`. 
+   Store all teas before `"chai"` in a new array named `selectedTeas`.
+*/
+
+let teas = ["green tea", "black tea", "chai", "oolong tea"];
+let selectedTeas = [];
+
+for (let i = 0; i < teas.length; i++) {
+  if (teas[i] === "chai") {
+    break;
+  }
+  selectedTeas.push(teas[i]);
+}
+// console.log(selectedTeas);
