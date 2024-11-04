66b20b982d3e53a8973938e32558a6b7c7d4c200
rao-mukul <mukultheprogrammer@gmail.com>
2024-11-04 23:53:35 +0530

    level up challenge 2: continue keyword

--- jsFoundation/part4/levelUpChallenges.js
+++ jsFoundation/part4/levelUpChallenges.js
@@ -13,3 +13,19 @@ for (let i = 0; i < teas.length; i++) {
   selectedTeas.push(teas[i]);
 }
 // console.log(selectedTeas);
+
+/* 
+2. Write a `for` loop that loops through the array `["London", "New York", "Paris", "Berlin"]` and skips `"Paris"`. 
+   Store the other cities in a new array named `visitedCities`.
+*/
+
+let cities = ["London", "New York", "Paris", "Berlin"];
+let visitedCities = [];
+
+for (let i = 0; i < cities.length; i++) {
+  if (cities[i] === "Paris" || cities[i] === "paris") {
+    continue;
+  }
+  visitedCities.push(cities[i]);
+}
+// console.log(visitedCities);
