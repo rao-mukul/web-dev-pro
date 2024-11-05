7816a8b78d42657e7561af12ae882d80421932e4
rao-mukul <mukultheprogrammer@gmail.com>
2024-11-05 23:06:38 +0530

    function in js: classic function

---
+++ jsFoundation/part5/function.js
@@ -0,0 +1,11 @@
+/* 
+1. Write a function named `makeTea` that takes one parameter, `typeOfTea`, and returns a string like `"Making green tea"` when called with `"green tea"`. 
+Store the result in a variable named `teaOrder`.
+*/
+
+function makeTea(typeOfTea) {
+  return `Making ${typeOfTea}`;
+}
+
+const teaOrder = makeTea("Ice Tea");
+// console.log(teaOrder);
