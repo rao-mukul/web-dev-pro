8ce04af85935beddb536177bb8e7bff542c52304
rao-mukul <mukultheprogrammer@gmail.com>
2024-11-05 23:18:05 +0530

    function in js: arrow function

--- jsFoundation/part5/function.js
+++ jsFoundation/part5/function.js
@@ -26,3 +26,18 @@ const orderConfirmation = orderTea("Chai");
 // console.log(orderConfirmation);
 
 // There's an Execution Context Associated with every Function
+
+/* 
+3. Write an arrow function named `calculateTotal` that takes two parameters: `price` and `quantity`. The function should return the total cost by multiplying the `price` and `quantity`. 
+Store the result in a variable named `totalCost`.
+*/
+
+const calculateTotal = (price, quantity) => {
+  return price * quantity;
+};
+
+// Another Shortcut way when we have only one statement in the function
+const calculateTotal2 = (price, quantity) => price * quantity;
+// Here no {} and return statement is required but the code must be in single line
+const totalCost = calculateTotal2(399, 1000);
+// console.log(totalCost);
