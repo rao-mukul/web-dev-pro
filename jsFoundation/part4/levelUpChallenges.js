9b121b0d03832ab06f480b566051518dc049a963
rao-mukul <mukultheprogrammer@gmail.com>
2024-11-05 14:56:46 +0530

    levelUpChallenges 9-10: Complete

--- jsFoundation/part4/levelUpChallenges.js
+++ jsFoundation/part4/levelUpChallenges.js
@@ -164,3 +164,44 @@ myWorldCities.forEach((city) => {
 });
 
 // console.log(traveledCities);
+
+/* 
+9. Write a `for` loop that iterates through the array `[2, 5, 7, 9]`. 
+   Skip the value `7` and multiply the rest by 2. Store the results in a new array named `doubledNumbers`.
+*/
+
+let myNumbers = [2, 5, 7, 9];
+let doubledNumbers = [];
+
+for (let i = 0; i < myNumbers.length; i++) {
+  if (myNumbers[i] === 7) {
+    continue;
+  }
+  doubledNumbers.push(myNumbers[i] * 2);
+}
+// console.log(doubledNumbers);
+
+/* 
+10. Use a `for-of` loop to iterate through the array `["chai", "green tea", "black tea", "jasmine tea", "herbal tea"]` 
+    and stop when the length of the current tea name is greater than 10. 
+    Store the teas iterated over in an array named `shortTeas`.
+*/
+
+let myTeasCollection = [
+  "chai",
+  "green tea",
+  "black tea",
+  "jasmine tea",
+  "herbal tea",
+];
+
+let shortTeas = [];
+
+for (const tea of myTeasCollection) {
+  if (tea.length > 10) {
+    break;
+  }
+  shortTeas.push(tea);
+}
+
+// console.log(shortTeas);
