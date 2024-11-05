90484b84e9f7131c09576418202140f58a506212
rao-mukul <mukultheprogrammer@gmail.com>
2024-11-05 13:15:04 +0530

    level up challenge 3-4 : for-of loop

--- jsFoundation/part4/levelUpChallenges.js
+++ jsFoundation/part4/levelUpChallenges.js
@@ -29,3 +29,35 @@ for (let i = 0; i < cities.length; i++) {
   visitedCities.push(cities[i]);
 }
 // console.log(visitedCities);
+
+/* 
+3. Use a `for-of` loop to iterate through the array `[1, 2, 3, 4, 5]` and stop when the number `4` is found. 
+   Store the numbers before `4` in an array named `smallNumbers`.
+*/
+
+let numbers = [1, 2, 3, 4, 5];
+smallNumbers = [];
+
+for (const number of numbers) {
+  if (number === 4) {
+    break;
+  }
+  smallNumbers.push(number);
+}
+// console.log(smallNumbers);
+
+/* 
+4. Use a `for-of` loop to iterate through the array `["chai", "green tea", "herbal tea", "black tea"]` and skip `"herbal tea"`. 
+   Store the other teas in an array named `preferredTeas`.
+*/
+
+let myTeas = ["chai", "green tea", "herbal tea", "black tea"];
+let preferredTeas = [];
+
+for (const tea of myTeas) {
+  if (tea === "herbal tea") {
+    continue;
+  }
+  preferredTeas.push(tea);
+}
+// console.log(preferredTeas);
