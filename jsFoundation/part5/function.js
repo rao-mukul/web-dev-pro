bba79559f9bb89beef5dd89cfda10b4e169393f5
rao-mukul <mukultheprogrammer@gmail.com>
2024-11-05 23:11:52 +0530

    function in js: nested functions

--- jsFoundation/part5/function.js
+++ jsFoundation/part5/function.js
@@ -9,3 +9,20 @@ function makeTea(typeOfTea) {
 
 const teaOrder = makeTea("Ice Tea");
 // console.log(teaOrder);
+
+/* 
+2. Create a function named `orderTea` that takes one parameter, `teaType`. Inside this function, create another function named `confirmOrder` that returns a message like `"Order confirmed for chai"`. 
+Call `confirmOrder` from within `orderTea` and return the result.
+*/
+
+function orderTea(teaType) {
+  function confirmOrder(tea) {
+    return `Order confirmed for ${tea}`;
+  }
+  return confirmOrder(teaType);
+}
+
+const orderConfirmation = orderTea("Chai");
+// console.log(orderConfirmation);
+
+// There's an Execution Context Associated with every Function
