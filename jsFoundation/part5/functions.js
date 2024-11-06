6a0cf3ab5c800f34139ecd6d1ad033a7b164edaa
rao-mukul <mukultheprogrammer@gmail.com>
2024-11-06 12:00:09 +0530

    function in js: Higher Order Functions

--- jsFoundation/part5/functions.js
+++ jsFoundation/part5/functions.js
@@ -25,7 +25,7 @@ function orderTea(teaType) {
 const orderConfirmation = orderTea("Chai");
 // console.log(orderConfirmation);
 
-// There's an Execution Context Associated with every Function
+// There's an ---Execution Context--- Associated with every Function
 
 /* 
 3. Write an arrow function named `calculateTotal` that takes two parameters: `price` and `quantity`. The function should return the total cost by multiplying the `price` and `quantity`. 
@@ -42,6 +42,7 @@ const calculateTotal2 = (price, quantity) => price * quantity;
 const totalCost = calculateTotal2(399, 1000);
 // console.log(totalCost);
 
+// -----------------------------------------------------------------
 // This and Context in JS : Run the Following code in Bowser Console
 function testOne() {
   console.log(this);
@@ -53,11 +54,11 @@ function testOne() {
 const testTwo = () => {
   console.log(this);
 };
-testTwo(); // Arrow Function still prints the Window Object in Browser
+// testTwo(); // Arrow Function still prints the Window Object in Browser
 // Therefore, Arrow Function do have the this context ✅
 // So, the whole theory that Arrow functions don't have the this context is not true actually
 
-// Array Function do have the Ability to reach out the this context
+// Arrow Function do have the Ability to reach out the this context
 // But It doesn't have the ability to Pass on this context anywhere
 
 // Eg: When we go into the DOM (Document Object Model):
@@ -67,3 +68,31 @@ testTwo(); // Arrow Function still prints the Window Object in Browser
 // The Whole Reason Behind them is :
 // That there is not enough ability in this in the Arrow Function
 // It is there but the ability to just pass it on this is not there
+
+// -----------------------------------------------------------------
+
+/* 
+4. Write a function named `processTeaOrder` that takes another function, `makeTea`, as a parameter and calls it with the argument `"earl grey"`. 
+Return the result of calling `makeTea`.
+*/
+
+function makeTea(teaType) {
+  return `makeTea: ${teaType}`;
+}
+
+function processTeaOrder(teaFunction) {
+  return teaFunction("earl grey");
+}
+
+const order = processTeaOrder(makeTea);
+// console.log(order);
+
+// These are Higher Order Functions
+// Also reffered as First-class Function in MDN docs
+// A programming language is said to have First-class functions when functions in that language are treated like any other variable. For example, in such a language, a function can be passed as an argument to other functions, can be returned by another function and can be assigned as a value to a variable.
+
+// forEach loop is also an example of Higher Order Function:
+// As it is a method which accepts further a function as it's parameter
+
+// 👉 Anything that takes function as a parameter or returns a function back
+// is an example of Higher order function
