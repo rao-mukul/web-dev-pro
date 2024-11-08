4dcf8f1812c2a9875792e4358d9ce9a223526da9
rao-mukul <mukultheprogrammer@gmail.com>
2024-11-08 20:37:02 +0530

    inject custom devLength property in Array prototype

--- jsFoundation/part6/prototypes.js
+++ jsFoundation/part6/prototypes.js
@@ -7,6 +7,7 @@ let lenovo = {
 let tomHardware = {};
 
 // console.log(`lenovo`, lenovo.__proto__);
+// console.log(lenovo.cpu);
 
 // Another better way of using Prototypes
 
@@ -18,5 +19,17 @@ let tesla = {
 
 Object.setPrototypeOf(tesla, genericCar);
 
+// console.log(tesla.tyres);
 // console.log(`tesla`,tesla.__proto__); // Previous Way
-console.log(`tesla`, Object.getPrototypeOf(tesla)); // Better Way
+// console.log(`tesla`, Object.getPrototypeOf(tesla)); // Better Way
+
+// -----inject property devLength in Array which gives the length counting from 0-----
+Object.defineProperty(Array.prototype, "devLength", {
+  get: function () {
+    return this.length - 1;
+  },
+});
+
+let arr = [1, 2, 3, 4];
+console.log(`Normal length: ${arr.length}`); // Output will be 4
+console.log(`devLength: ${arr.devLength}`); // Output will be 3
