a81afcf4b2f2e42dcfd817038e0274f6bccbb957
rao-mukul <mukultheprogrammer@gmail.com>
2024-11-11 19:17:26 +0530

    10 challenges in DOM : 6-10

--- jsFoundation/part8/scripts.js
+++ jsFoundation/part8/scripts.js
@@ -18,12 +18,14 @@ document
 document.getElementById("changeOrder").addEventListener("click", function () {
   let coffeeType = document.getElementById("coffeeType");
   coffeeType.textContent = "Espresso";
+  coffeeType.style.backgroundColor = "brown";
+  coffeeType.style.padding = "5px";
 });
 
 // Example 4
 document.getElementById("addNewItem").addEventListener("click", function () {
   let newItem = document.createElement("li");
-  newItem.textContent = "Egg";
+  newItem.textContent = "Eggs";
   let shoppingList = document.getElementById("shoppingList");
   shoppingList.appendChild(newItem);
 });
@@ -34,4 +36,44 @@ document
   .addEventListener("click", function () {
     let taskList = document.getElementById("taskList");
     taskList.lastElementChild.remove();
+  });
+
+// Example 6
+document
+  .getElementById("clickMeButton")
+  .addEventListener("dblclick", function () {
+    alert("Event Handling in the DOM");
+  });
+
+// Example 7
+document.getElementById("teaList").addEventListener("click", function (event) {
+  alert(`You Clicked on: ${event.target.textContent}`);
+  event.target.classList.add("highlight");
+});
+
+// Example 8
+document
+  .getElementById("feedbackForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); //prevent the default submit behaviour
+    let feedbackInput = document.getElementById("feedbackInput");
+    let feedbackDisplay = document.getElementById("feedbackDisplay");
+    feedbackDisplay.textContent = `Feedback is: ${feedbackInput.value}`;
+    // Note the use of "value" here instead of "textContent" for the input element
+    feedbackDisplay.style.fontSize = "25px";
+    feedbackDisplay.classList.add("highlight");
+  });
+
+// Example 9
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("domStatus").textContent = "DOM fully Loaded";
+});
+
+// Example 10
+document
+  .getElementById("toggleHighlight")
+  .addEventListener("click", function () {
+    let descriptionText = document.getElementById("descriptionText");
+    descriptionText.classList.toggle("highlight");
+    descriptionText.style.border = "4px solid pink";
   });
