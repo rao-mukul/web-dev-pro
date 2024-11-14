240199aeb94df906a10f6856ba7392053435f31b
rao-mukul <mukultheprogrammer@gmail.com>
2024-11-14 13:13:51 +0530

    ES6 modules

---
+++ jsFoundation/part9_adv/ES6_modules&commonJS/ES6_modules/app.js
@@ -0,0 +1,12 @@
+// Module Import File
+
+// default import
+import multiply from "./mathOperationsM.js";
+
+
+
+// named import
+import { add, subtract } from "./mathOperationsM.js";
+
+console.log(multiply(2, 4));
+console.log(subtract(9, 3));
