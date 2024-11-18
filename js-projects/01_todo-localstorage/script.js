5430917403022ddddff7fd44d39db0e96da5b193
rao-mukul <mukultheprogrammer@gmail.com>
2024-11-18 23:43:14 +0530

    handle local storage and DOM event

--- js-projects/01_todo-localstorage/script.js
+++ js-projects/01_todo-localstorage/script.js
@@ -4,13 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const todoList = document.getElementById("todo-list");
 
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
   tasks.forEach((task) => renderTask(task));
 
   addTaskButton.addEventListener("click", () => {
     const taskText = todoInput.value.trim();
-    if (taskText === "") {
-      return;
-    }
+    if (taskText === "") return;
     const newTask = {
       id: Date.now(),
       text: taskText,
@@ -19,6 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
     tasks.push(newTask);
     saveTasks();
     todoInput.value = ""; // clear input
+    console.log(tasks);
   });
 
   function renderTask(task) {
