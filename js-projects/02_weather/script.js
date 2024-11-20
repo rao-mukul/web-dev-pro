6a5e28005fafbb122df68fd0ff53c9cf99283b1d
rao-mukul <mukultheprogrammer@gmail.com>
2024-11-20 23:32:18 +0530

    weather app: get required elements

---
+++ js-projects/02_weather/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cityInput = document.getElementById("city-input");
+  const getWeatherBtn = document.getElementById("get-weather-btn");
+  const weatherInfo = document.getElementById("weather-info");
+  const cityNameDisplay = document.getElementById("city-name");
+  const temperatureDisplay = document.getElementById("temperature");
+  const descriptionDisplay = document.getElementById("description");
+  const errorMessage = document.getElementById("error-message");
+
+  getWeatherBtn.addEventListener("click", () => {
+    const city = cityInput.ariaValueMax.trim();
+    if (!city) return;
+  });
+});
