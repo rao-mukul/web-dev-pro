53527a979c89f4719a906ec306f68f6c2d754e45
rao-mukul <mukultheprogrammer@gmail.com>
2024-11-20 23:55:20 +0530

    add function declaration & basic structure

--- js-projects/02_weather/script.js
+++ js-projects/02_weather/script.js
@@ -6,9 +6,33 @@ document.addEventListener("DOMContentLoaded", () => {
   const temperatureDisplay = document.getElementById("temperature");
   const descriptionDisplay = document.getElementById("description");
   const errorMessage = document.getElementById("error-message");
+  const API_KEY = "589dcbceec9d02b60b376891665553f4"; //env variables
 
-  getWeatherBtn.addEventListener("click", () => {
+  getWeatherBtn.addEventListener("click", async () => {
     const city = cityInput.ariaValueMax.trim();
     if (!city) return;
+
+    // it may throw an error
+    // server/database is always in another continent
+
+    try {
+      const weatherData = await fetchWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (error) {
+      showError();
+    }
   });
+
+  async function fetchWeatherData(city) {
+    // gets the data
+  }
+
+  function displayWeatherData(fetchWeatherData) {
+    // display
+  }
+
+  function showError() {
+    weatherInfo.classList.add("hidden");
+    errorMessage.classList.remove("hidden");
+  }
 });
